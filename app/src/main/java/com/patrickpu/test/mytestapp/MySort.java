d49b4b6eb4871eabff330860e3481ce1abf157ac
Patrick Pu <patrickpu@fortigofreight.com>
2018-02-13 11:29:18 -0500

    insertion sort ok

--- app/src/main/java/com/patrickpu/test/mytestapp/MySort.java
+++ app/src/main/java/com/patrickpu/test/mytestapp/MySort.java
@@ -1,18 +1,14 @@
 package com.patrickpu.test.mytestapp;
 
-/**
- * Created by patrickpu on 2/13/2018.
- */
+class MySort {
 
-public class MySort {
-
-    public static int[] bubbleSort(int[] array, Key key){
+    static int[] bubbleSort(int[] array, Key key){
         int i,j;
 
         for (i=0; i<array.length-1; i++){
             for (j=0; j<array.length-1-i; j++){
                 if ( (key == Key.ASC && array[j] > array[j+1]) || (key == Key.DESC && array[j] < array[j + 1])){
-                    swap(array, i, j);
+                    swap(array, j, j+1);
                 }
             }
         }
@@ -20,10 +16,26 @@ public class MySort {
         return array;
     }
 
-    public static void swap(int[] array, int i, int j){
+    static int[] insertionSort(int[] array){
+        int i,j,cur;
+
+        for (i=1; i<array.length; i++){
+            cur = array[i];
+            j = i-1;
+
+            while(j>=0&&array[j]>cur){
+                swap(array, j, j+1);
+                j--;
+            }
+        }
+
+        return array;
+    }
+
+    private static void swap(int[] array, int i, int j){
         int temp;
         temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
+        array[j] = array[i];
+        array[i] = temp;
     }
 }
--- app/src/test/java/com/patrickpu/test/mytestapp/MySortUnitTest.java
+++ app/src/test/java/com/patrickpu/test/mytestapp/MySortUnitTest.java
@@ -3,15 +3,11 @@ package com.patrickpu.test.mytestapp;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * Created by patrickpu on 2/13/2018.
- */
-
 public class MySortUnitTest {
 
-    int[] testArray = new int[]{3,8,6,9,2,4};
-    int[] expectedArrayAsc = new int[]{2,3,4,6,8,9};
-    int[] expectedArrayDesc = new int[]{9,8,6,4,3,2};
+    private int[] testArray = new int[]{3,8,6,9,2,3};
+    private int[] expectedArrayAsc = new int[]{2,3,3,6,8,9};
+    private int[] expectedArrayDesc = new int[]{9,8,6,3,3,2};
 
     @Test
     public void testBubbleSortAsc(){
@@ -20,6 +16,10 @@ public class MySortUnitTest {
     @Test
     public void testBubbleSortDesc(){
         assertArrayEquals(expectedArrayDesc, MySort.bubbleSort(testArray, Key.DESC));
+    }
+    @Test
+    public void testInsertionSort(){
+        assertArrayEquals(expectedArrayAsc, MySort.insertionSort(testArray));
     }
 
 }
